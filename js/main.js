7404f4a2e9aa7ea133c863d6e8215ff7cb2a115a
SebastianMestre <mestresebastianunr@gmail.com>
2019-08-31 20:06:37 -0300

    Refactor zarpado

--- js/main.js
+++ js/main.js
@@ -1,20 +1,71 @@
-
-
-const renderizarPedido = (numero, estado) =>
-`<div class="orden">
-    <img src="comida.png" alt="icono de comida">
-    <div class="numero-de-orden">#${numero}</div>
-    <div class="estado-de-orden">${estado}</div>
-</div>`;
-
-const agregarPedido = (numero, estado) => {
-    document.body.innerHTML += renderizarPedido(numero, estado);
+const textoAElementoHTML = texto => {
+	let template = document.createElement('template');
+	template.innerHTML = texto.trim();
+	return template.content.firstChild;
 };
 
-/*
+class ListaDePedidos {
+	constructor (elementoContenedor) {
+		this.pedidos = new Map();
+		this.elementoContenedor = elementoContenedor;
+	}
 
+	agregar (pedido) {
+		let numeroDePedido = pedido.numero;
+
+		if(this.pedidos.has(numeroDePedido))
+			this.borrar(numeroDePedido);
+
+		this.pedidos.set(numeroDePedido, pedido);
+
+		this.elementoContenedor.appendChild(pedido.elemento);
+	}
+
+	borrar (numeroDePedido) {
+		if (!this.pedidos.has(numeroDePedido))
+			return;
+
+		this.pedidos.get(numeroDePedido).elemento.remove();
+
+		this.pedidos.delete(numeroDePedido);
+	}
+}
+
+class Pedido {
+	constructor (numero, estado) {
+		this.numero = numero;
+		this.estado = estado;
+
+		this.elemento = this.render();
+	}
+	
+	resaltar () {
+		this.elemento.classList.add("resaltado")
+	}
+
+	borrar () {
+		this.elemento.classList.add("borrando");
+		setTimeout(_ => {
+			this.elemento.remove();
+		}, 990);
+	}
+
+	render () {
+		return textoAElementoHTML(this.html());
+	}
+
+	html () {
+		return "" +
+`<div class="orden">
+    <img src="comida.png" alt="icono de comida">
+    <div class="numero-de-orden">#${this.numero}</div>
+    <div class="estado-de-orden">${this.estado}</div>
+</div>`;
+	}
+}
+
+
+let pedidos = null;
 window.addEventListener("load", e => {
-    // TODO: agregar setInterval que lea del servidor
+	pedidos = new ListaDePedidos(document.body);
 });
-
-*/
