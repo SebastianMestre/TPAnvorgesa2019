005269421e400ff24b3d95fdcaf64a4a4f8af2bf
SebastianMestre <mestresebastianunr@gmail.com>
2019-09-05 18:49:36 -0300

    Agrega una funcion para hacer xhr

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -5,8 +5,18 @@ const textoAElementoHTML = texto => {
 	return template.content.firstChild;
 };
 
-const delay = (time) => {
+const delay = time => {
 	return new Promise((success, reject) => {
 		setTimeout(success, time);
 	});
-}
+};
+
+const xhrGet = address => new Promise((resolve, reject) => { 
+	const req = new XMLHttpRequest();
+	req.addEventListener("load", _ => {
+		if (req.status === 200) resolve(req.responseText);
+		else                    reject(req.responseText);
+	});
+	req.open("GET", address);
+	req.send()
+});
