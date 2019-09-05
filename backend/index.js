ea3083ca82cc08ae046047987fdcc308fc126e47
SebastianMestre <mestresebastianunr@gmail.com>
2019-09-05 17:49:51 -0300

    Agrega algunas cosas al backend

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,41 @@
 let express = require('express');
 
 let app = express();
+app.use(express.json());
+
+let data = [];
 
 app.get('/read', (req, res) => {
-	res.send('[]');
+	res.send(data);
 });
 
-app.post('/delete', (a, b) => {
+
+app.post('/assign', (req, res) => {
+	for (let item of req.body) {
+		data.push(item);
+	}
+	res.send();
+});
+
+app.post('/update', (req, res) => {
 
 });
 
-app.post('/assign', (a, b) => {
+app.post('/insert', (req, res) => {
 
 });
 
-app.post('/update', (a, b) => {
+
+app.post('/delete', (req, res) => {
+	data = data.filter(datum =>
+		req.body.none(item =>
+			item.id === datum.id));
+});
+
+app.post('/clear', (req, res) => {
 
 });
 
-app.post('/insert', (a, b) => {
-
-});
 
 app.listen(8100, _ => {
 	console.log("Listening");
