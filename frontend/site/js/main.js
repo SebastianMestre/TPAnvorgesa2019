145e1b17c0573ff41d894b468f769a2452f76c39
SebastianMestre <mestresebastianunr@gmail.com>
2019-09-15 11:02:45 -0300

    Reestructura los archivos del front end para agregar la interfaz para agregar y borrar pedidos

--- frontend/site/js/main.js
+++ frontend/site/js/main.js
@@ -1,5 +1,6 @@
 
 let pedidos = null;
+
 window.addEventListener("load", e => {
 	pedidos = new ListaDePedidos(document.body);
 
@@ -7,6 +8,5 @@ window.addEventListener("load", e => {
 		xhrGet("http://localhost:8100/read")
 			.then(JSON.parse)
 			.then(data => pedidos.asignar(data));
-
-	}, 2000);
+	}, 10000);
 });
