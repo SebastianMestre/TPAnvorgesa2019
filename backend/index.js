d6930d13b4731899c62b3bfbf9a1ee0bb1696986
SebastianMestre <mestresebastianunr@gmail.com>
2019-09-15 13:27:21 -0300

    Toca un poco el backend

--- backend/index.js
+++ backend/index.js
@@ -11,31 +11,40 @@ app.use((req, res, next) => {
 let data = [];
 
 app.get('/read', (req, res) => {
+	// req.send toma lo queremos mandar al front end.
+	// read simplemente envia el array data.
 	res.send(data);
 });
 
 
 app.post('/assign', (req, res) => {
+	// req.body contiene un array con el input
 	for (let item of req.body) {
 		data.push(item);
 	}
+
+	// req.send toma lo queremos mandar al front end, en este caso nada.
 	res.send();
 });
 
+// Hay pedidos (identificados por id) que ya estan en el array
+// pero queremos cambiar algunos de sus valores.
+// Update itera y actualiza estos valores
 app.post('/update', (req, res) => {
-	//No sé a qué se refiere con update
+	res.send();
 });
 
 app.post('/insert', (req, res) => {
-	data.push(/*¿Qué concha va acá? ¿De dónde saco yo el input del pedido?*/);
-	//res.send(orders); Pero, ¿por qué?
+	res.send();
 });
 
 
 app.post('/delete', (req, res) => {
+	let items = [...req.body];
 	data = data.filter(datum =>
-		req.body.none(item =>
-			item.id === datum.id));
+		items.every(item =>
+			item.id !== datum.id));
+	res.send();
 });
 
 app.post('/clear', (req, res) => {
